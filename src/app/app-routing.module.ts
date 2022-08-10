7b4279177d50268b327664a7c9d0464dd3aabc6f
diegobizerra <diegobizerra@tjrj.jus.br>
2022-08-10 19:59:35 -0300

    Criação do front cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cadastro-clientes', component:CadastroClientesComponent},
+  {path:'consulta-clientes', component:ConsultaClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroClientesComponent } from './cadastro-clientes/cadastro-clientes.component';
+import { ConsultaClientesComponent } from './consulta-clientes/consulta-clientes.component';
+//importando as classes para uso da biblioteca de formularios do angular
+import{FormsModule, ReactiveFormsModule}from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClientesComponent,
+    ConsultaClientesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,//Registrando biblioteca para formulário
+    ReactiveFormsModule //Registrando biblioteca para formularios
   ],
   providers: [],
   bootstrap: [AppComponent]
